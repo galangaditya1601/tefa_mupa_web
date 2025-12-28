0c316b521a2fb65aa9fe8b58be777b12ceb8e1a7
galangaditya1601 <galangadityapratama5@gmail.com>
2025-12-29 00:01:40 +0700

    Add backoffice route prefix to web routes

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,14 @@ use Illuminate\Support\Facades\Route;
 // Halaman Utama
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/produk/{slug}', [ProductController::class, 'show'])->name('products.show');
+
+
+
+// backoffice
+Route::prefix('backoffice')->group(function () {
+    // Tambahkan rute backoffice di sini
+});
+
+
+
+
